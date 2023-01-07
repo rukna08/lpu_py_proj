7290dde26fa7a7cc1b5507d5055e3ad120e545af
Ankur Banerjee <rukna2003@gmail.com>
2023-01-07 15:14:05 +0530

    -

---
+++ scripts/run_quiz.bat
@@ -0,0 +1,7 @@
+@echo off
+
+pushd ..
+
+python src/quiz.py
+
+popd
---
+++ src/quiz.py
@@ -0,0 +1,4 @@
+questions_array = []
+
+with open("res/questions.txt") as questions_txtfile:
+    questions_array = questions_txtfile.readlines()
