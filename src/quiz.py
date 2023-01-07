4f221032fad6ed7fb2764eb03308b2c240b6eafb
Ankur Banerjee <rukna2003@gmail.com>
2023-01-07 16:52:27 +0530

    -

--- src/quiz.py
+++ src/quiz.py
@@ -10,20 +10,26 @@ def display_question(surface, question_index):
     quiz_font.render_to(surface, (20, 20), questions_array[question_index], (0, 0, 0))
 
 def display_options_num(surface):
-    quiz_font.render_to(surface, (20, 100), "1.  ", (0, 0, 0))
-    quiz_font.render_to(surface, (20, 150), "2.  ", (0, 0, 0))
-    quiz_font.render_to(surface, (20, 200), "3.  ", (0, 0, 0))
-    quiz_font.render_to(surface, (20, 250), "4.  ", (0, 0, 0))
+    quiz_font.render_to(surface, (50, 100), "1.  ", (0, 0, 0))
+    quiz_font.render_to(surface, (50, 150), "2.  ", (0, 0, 0))
+    quiz_font.render_to(surface, (50, 200), "3.  ", (0, 0, 0))
+    quiz_font.render_to(surface, (50, 250), "4.  ", (0, 0, 0))
 
 def display_options(surface, options_index):
-    quiz_font.render_to(surface, (50, 100), options_array[options_index], (0, 0, 0))
-    quiz_font.render_to(surface, (50, 150), options_array[options_index + 1], (0, 0, 0))
-    quiz_font.render_to(surface, (50, 200), options_array[options_index + 2], (0, 0, 0))
-    quiz_font.render_to(surface, (50, 250), options_array[options_index + 3], (0, 0, 0))
+    quiz_font.render_to(surface, (80, 100), options_array[options_index], (0, 0, 0))
+    quiz_font.render_to(surface, (80, 150), options_array[options_index + 1], (0, 0, 0))
+    quiz_font.render_to(surface, (80, 200), options_array[options_index + 2], (0, 0, 0))
+    quiz_font.render_to(surface, (80, 250), options_array[options_index + 3], (0, 0, 0))
 
 def remove_newline_from_string_array(arr):
     for i in range(len(arr)):
         arr[i] = arr[i].strip()
+
+def draw_tickbox(surface):
+    pygame.draw.rect(surface, black_color, (15, 100, 20, 20), 1)
+    pygame.draw.rect(surface, black_color, (15, 150, 20, 20), 1)
+    pygame.draw.rect(surface, black_color, (15, 200, 20, 20), 1)
+    pygame.draw.rect(surface, black_color, (15, 250, 20, 20), 1)
 
 questions_array = []
 options_array = []
@@ -37,9 +43,10 @@ with open("res/options.txt") as options_txtfile:
 remove_newline_from_string_array(questions_array)
 remove_newline_from_string_array(options_array)
 
-white_colour = (255, 255, 255)
+white_color = (255, 255, 255)
+black_color = (0, 0, 0)
 
-width = 800
+width = 1200
 
 height = 500
 
@@ -53,13 +60,15 @@ question_index = 0
 options_index = 0
 
 while running:
-    surface.fill(white_colour)
+    surface.fill(white_color)
 
     display_question(surface, question_index)
 
     display_options_num(surface)
     
     display_options(surface, options_index)
+
+    draw_tickbox(surface)
 
     pygame.display.flip()
 
