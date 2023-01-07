4da1acd3e82924918e088567d47352c72581ab19
Ankur Banerjee <rukna2003@gmail.com>
2023-01-07 15:37:39 +0530

    -

--- src/quiz.py
+++ src/quiz.py
@@ -9,6 +9,12 @@ quiz_font = pygame.freetype.Font('res/Roboto-Regular.ttf', 27)
 def display_question(surface, question_index, coordinate):
     quiz_font.render_to(surface, coordinate, questions_array[question_index], (0, 0, 0))
 
+def display_options(surface):
+    quiz_font.render_to(surface, (20, 100), "1.  ", (0, 0, 0))
+    quiz_font.render_to(surface, (20, 150), "2.  ", (0, 0, 0))
+    quiz_font.render_to(surface, (20, 200), "3.  ", (0, 0, 0))
+    quiz_font.render_to(surface, (20, 250), "4.  ", (0, 0, 0))
+
 questions_array = []
 
 with open("res/questions.txt") as questions_txtfile:
@@ -32,6 +38,8 @@ while running:
     surface.fill(white_colour)
 
     display_question(surface, question_index, (20, 20))
+
+    display_options(surface)
     
     pygame.display.flip()
 
