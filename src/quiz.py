dbbf5070334174dd66b982de8a05a8857d8d7388
Ankur Banerjee <rukna2003@gmail.com>
2023-01-07 15:34:29 +0530

    -

--- src/quiz.py
+++ src/quiz.py
@@ -1,4 +1,49 @@
+import pygame
+
+pygame.init()
+
+pygame.font.init()
+
+quiz_font = pygame.freetype.Font('res/Roboto-Regular.ttf', 27)
+
+def display_question(surface, question_index, coordinate):
+    quiz_font.render_to(surface, coordinate, questions_array[question_index], (0, 0, 0))
+
 questions_array = []
 
 with open("res/questions.txt") as questions_txtfile:
     questions_array = questions_txtfile.readlines()
+
+white_colour = (255, 255, 255)
+
+width = 800
+
+height = 500
+
+surface = pygame.display.set_mode((width, height))
+
+pygame.display.set_caption("Quiz")
+
+running = True
+
+question_index = 0
+
+while running:
+    surface.fill(white_colour)
+
+    display_question(surface, question_index, (20, 20))
+    
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                if question_index + 1 != len(questions_array):
+                    question_index += 1
+
+            if event.key == pygame.K_LEFT:
+                if question_index - 1 != -1:
+                    question_index -= 1
